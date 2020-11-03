fd2da4c165544ca03cf3ace7ca48b5ecc32fc2d7
Abiraman K <abiramancit@gmail.com>
2020-11-04 00:21:59 +0530

    Adds firebase push notification to foreground

--- App.tsx
+++ App.tsx
@@ -1,118 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
+import React, { Component } from "react";
+import { 
+  Alert, 
   View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  Text
+} from "react-native";
+import AsyncStorage from '@react-native-community/async-storage';
+import firebase from "react-native-firebase";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
 
-declare const global: {HermesInternal: null | {}};
+  notificationListener: any;
+  notificationOpenedListener: any;
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  async componentDidMount() {
+    //we check if user has granted permission to receive push notifications.
+    this.checkPermission();
+    // Register all listener for notification 
+    this.createNotificationListeners();
+  }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    // If Premission granted proceed towards token fetch
+    if (enabled) {
+      this.getToken();
+    } else {
+      // If permission hasn’t been granted to our app, request user in requestPermission method. 
+      this.requestPermission();
+    }
+  }
+
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+
+    console.log("FCM token", fcmToken)
+  }
+
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+    }
+  }
+
+  async createNotificationListeners() {
+
+    // This listener triggered when notification has been received in foreground
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      console.log("title", title)
+      console.log("body", body)
+      console.log("Notification open foreground", notificationOpen.notification)
+      this.displayNotification(title, body);
+    });
+
+    // This listener triggered when app is in backgound and we click, tapped and opened notifiaction
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      console.log("title", title)
+      console.log("body", body)
+      console.log("Notification open background", notificationOpen.notification)
+      this.displayNotification(title, body);
+    });
+
+    // This listener triggered when app is closed and we click,tapped and opened notification 
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      console.log("title", title)
+      console.log("body", body)
+      console.log("Notification open quit", notificationOpen.notification)
+      this.displayNotification(title, body);
+    }
+  }
+
+
+  displayNotification(title: string, body: string) {
+    // we display notification in alert box with title and body
+    Alert.alert(
+      title, body,
+      [
+        { text: 'Ok', onPress: () => console.log('ok pressed') },
+      ],
+      { cancelable: false },
+    );
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>React Native Push Notification</Text>
+      </View>
+    );
+  }
+}
 
 export default App;
